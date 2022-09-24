a4ce21f559f1a97066aaa58396ff2669b1341c87
ElianoCordeiro <98656854+Eliano098@users.noreply.github.com>
2022-09-24 17:15:37 -0300

    create project

---
+++ AssinaturaDigital/Home.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace AssinaturaDigital
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdAssinar = new System.Windows.Forms.Button();
+            this.txtCertificado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSalvar = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmdAssinar
+            // 
+            this.cmdAssinar.BackColor = System.Drawing.Color.DarkGray;
+            this.cmdAssinar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmdAssinar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmdAssinar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdAssinar.Location = new System.Drawing.Point(295, 290);
+            this.cmdAssinar.Margin = new System.Windows.Forms.Padding(4);
+            this.cmdAssinar.Name = "cmdAssinar";
+            this.cmdAssinar.Size = new System.Drawing.Size(164, 59);
+            this.cmdAssinar.TabIndex = 0;
+            this.cmdAssinar.Text = "Assinar";
+            this.cmdAssinar.UseVisualStyleBackColor = false;
+            this.cmdAssinar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCertificado
+            // 
+            this.txtCertificado.Location = new System.Drawing.Point(13, 190);
+            this.txtCertificado.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCertificado.Name = "txtCertificado";
+            this.txtCertificado.Size = new System.Drawing.Size(653, 23);
+            this.txtCertificado.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 169);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Insira o caminho do certificado digital";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(297, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PDF Digital Signer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 54);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Insira o caminho do documento";
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Location = new System.Drawing.Point(13, 75);
+            this.txtDocumento.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(653, 23);
+            this.txtDocumento.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::AssinaturaDigital.Properties.Resources.asppdf_550x550;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(624, 224);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(204, 199);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::AssinaturaDigital.Properties.Resources.pdf;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(701, 16);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 126);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 224);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(227, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Insira a senha do certificado digital";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(13, 245);
+            this.txtSenha.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(225, 23);
+            this.txtSenha.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 111);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(302, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Insira o caminho onde será salvo o documento";
+            // 
+            // txtSalvar
+            // 
+            this.txtSalvar.Location = new System.Drawing.Point(13, 132);
+            this.txtSalvar.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSalvar.Name = "txtSalvar";
+            this.txtSalvar.Size = new System.Drawing.Size(653, 23);
+            this.txtSalvar.TabIndex = 10;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(844, 365);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSalvar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDocumento);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCertificado);
+            this.Controls.Add(this.cmdAssinar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Assinatura Digital";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdAssinar;
+        private System.Windows.Forms.TextBox txtCertificado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSalvar;
+    }
+}
+
---
+++ AssinaturaDigital/Home.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aspose.Pdf;
+using Aspose.Pdf.Facades;
+using Aspose.Pdf.Forms;
+
+namespace AssinaturaDigital
+{
+    public partial class Home: System.Windows.Forms.Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Document pdfDocument = new Document(txtDocumento.Text);
+            PdfFileSignature signature = new PdfFileSignature(pdfDocument);
+            PKCS7 pkcs = new PKCS7(txtCertificado.Text, txtSenha.Text);
+            DocMDPSignature docMdpSignature = new DocMDPSignature(pkcs, DocMDPAccessPermissions.FillingInForms);
+            System.Drawing.Rectangle rect = new System.Drawing.Rectangle(100, 100, 600, 100);
+            signature.Certify(1, "Validação de teste", "Eliano Cordeiro", "Alfenas.MG", true, rect, docMdpSignature);
+            signature.Save(txtSalvar.Text+"Digitally Signed PDF.pdf");
+
+            MessageBox.Show("Assinado com sucesso!");
+            txtDocumento.Text = "";
+            txtCertificado.Text = "";
+            txtSenha.Text = "";
+            txtSalvar.Text = "";
+        }
+    }
+}
